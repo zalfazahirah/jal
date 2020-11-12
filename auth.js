3108c14b4f946de0d62325840c3375bfbfc57f5a
zalfazahirah <74364695+zalfazahirah@users.noreply.github.com>
2020-11-12 21:54:12 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
-   consumer_key: process.env.C_KEY,
+   consumer_key: AbRwejXOWA9Rla9EquUvQnjy7,
    consumer_secret: process.env.C_SECRET,
-   access_token: process.env.A_TOKEN,
-   access_token_secret: process.env.A_SECRET,
+   access_token:w9KAHsbhCra865ezkKDLAuxN6erdXmmpyhIEY5sfKiN4BniUc3,
+   access_token_secret:IVMaL2edX8z1DJCHA8N2KKL19waQiUewKD2MTvAHToZbS,
    timeout_ms: 60*1000
 }
